42a993a882387a24bd03a1f19c941aa4eadbd1e9
Jeremy Yallop <yallop@gmail.com>
2016-04-26 17:14:46 +0100

    Add support for FUSE 7.13

--- lib_gen/profuse_typegen.ml
+++ lib_gen/profuse_typegen.ml
@@ -33,6 +33,7 @@ let fuse_versions : (string * (string * (module Cstubs.Types.BINDINGS))) list = 
   "7_10", ("fuse_kernel.h.7_10", (module Profuse_types_7_10.C));
   "7_11", ("fuse_kernel.h.7_11", (module Profuse_types_7_11.C));
   "7_12", ("fuse_kernel.h.7_12", (module Profuse_types_7_12.C));
+  "7_13", ("fuse_kernel.h.7_13", (module Profuse_types_7_12.C));
 ]
 
 let resolve_version : string -> (string * (module Cstubs.Types.BINDINGS)) =
