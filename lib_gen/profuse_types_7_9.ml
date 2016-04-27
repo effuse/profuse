41d60a2ab3d0b8bb97f552925861d92cee61fa0d
Jeremy Yallop <yallop@gmail.com>
2016-04-27 12:05:36 +0100

    Add FUSE 7.9 flags (Init, Read, Write, Lk) and setattr/getattr constants.

--- lib_gen/profuse_types_7_9.ml
+++ lib_gen/profuse_types_7_9.ml
@@ -58,7 +58,6 @@ module C_compatible(F: Cstubs.Types.TYPE) = struct
     module V_7_8 = Version_7_8.In
     module Opcode = V_7_8.Opcode
     module Hdr       = struct include V_7_8.Hdr       let () = seal t end
-    module Init      = struct include V_7_8.Init      let () = seal t end
     module Release   = struct include V_7_8.Release   let () = seal t end
     module Access    = struct include V_7_8.Access    let () = seal t end
     module Forget    = struct include V_7_8.Forget    let () = seal t end
@@ -72,7 +71,24 @@ module C_compatible(F: Cstubs.Types.TYPE) = struct
     module Bmap      = struct include V_7_8.Bmap      let () = seal t end
     module Getxattr  = struct include V_7_8.Getxattr  let () = seal t end
     module Setxattr  = struct include V_7_8.Setxattr  let () = seal t end
+    module Init      = struct
+      module Flags =
+      struct
+        include V_7_8.Init.Flags
+        let fuse_file_ops = constant "FUSE_FILE_OPS" t
+        let fuse_atomic_o_trunc = constant "FUSE_ATOMIC_O_TRUNC" t
+        let fuse_big_writes = constant "FUSE_BIG_WRITES" t
+      end
+      include (V_7_8.Init
+               : module type of V_7_8.Init with module Flags := Flags)
+      let () = seal t
+    end
     module Read      = struct
+      module Flags =
+      struct
+        let t = uint32_t
+        let fuse_read_lockowner = constant "FUSE_READ_LOCKOWNER" t
+      end
       include V_7_8.Read
       let read_flags = "read_flags" -:* uint32_t
       let lock_owner = "lock_owner" -:* uint64_t
@@ -80,22 +96,46 @@ module C_compatible(F: Cstubs.Types.TYPE) = struct
       let () = seal t
     end
     module Write     = struct
+      module Flags =
+      struct
+        let t = uint32_t
+        let fuse_write_cache = constant "FUSE_WRITE_CACHE" t
+        let fuse_write_lockowner = constant "FUSE_WRITE_LOCKOWNER" t
+      end
       include V_7_8.Write
       let lock_owner = "lock_owner" -:* uint64_t
       let flags      = "flags"      -:* uint32_t
       let () = seal t
     end
     module Lk        = struct
+      module Flags =
+      struct
+        let t = uint32_t
+        let fuse_lk_flock = constant "FUSE_LK_FLOCK" t
+      end
       include V_7_8.Lk
       let lk_flags   = "lk_flags"   -:* uint32_t
       let () = seal t
     end
     module Setattr   = struct
-      include V_7_8.Setattr
+      module Valid =
+      struct
+        include V_7_8.Setattr.Valid
+        let fattr_atime_now = constant "FATTR_ATIME_NOW" t
+        let fattr_mtime_now = constant "FATTR_MTIME_NOW" t
+        let fattr_lockowner = constant "FATTR_LOCKOWNER" t
+      end
+      include (V_7_8.Setattr
+               : module type of V_7_8.Setattr with module Valid := Valid)
       let lock_owner     = "lock_owner" -:* uint64_t
       let () = seal t
     end
     module Getattr   = struct
+      module Flags =
+      struct
+        let t = uint32_t
+        let fuse_getattr_fh = constant "FUSE_GETATTR_FH" t
+      end
       type t
       let t : t structure typ = structure "fuse_getattr_in"
       let ( -:* ) s x = field t s x
